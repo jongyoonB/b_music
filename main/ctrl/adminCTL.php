26b797d2d3ab1dafebffad7c987c538b48d3394f
jongyoonB <jongyoonb@gmail.com>
2015-11-16 23:41:42 +0900

    initialize
    ing

--- main/ctrl/adminCTL.php
+++
@@ -1,7 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: JongYoon
- * Date: 2015-11-16
- * Time: 오전 10:54
- */
--- main/ctrl/playerCTL.php
+++
@@ -1,7 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: JongYoon
- * Date: 2015-11-16
- * Time: 오후 10:34
- */
